a4c79eb7d7a0da54b0a8e6a2a3d4d05442cbb5ae
harshved <harshved3@gmail.com>
2022-01-03 19:41:41 +0530

    Perfect

--- introapp/src/index.js
+++ introapp/src/index.js
@@ -1,11 +1,6 @@
 import react from 'react';
 import reactDOM from 'react-dom';
 
-reactDOM.render(<h1>Harsh
-  sfaasf
-  sdfsdf
-  ASfsdf
-  ASfsdffsfsds
-</h1>, 
+reactDOM.render(<h1>Harsh</h1>, 
   document.getElementById('root')
 );   
