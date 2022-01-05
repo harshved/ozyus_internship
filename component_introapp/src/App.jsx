9a07d615a0d62c0c6e67a7745259e4d6637b3a41
harshved <harshved3@gmail.com>
2022-01-05 17:50:37 +0530

    Component Created

---
+++ component_introapp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+import Bullet from "./Bullet";
+
+function App(){
+    return(
+        <>
+            <Heading/>
+            <Bullet/>
+        </>
+    )
+}
+
+export default App;
---
+++ component_introapp/src/Bullet.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Bullet(){
+    return (
+        <ol>
+            <li>Harsh</li>
+            <li>Vinay</li>
+            <li>Atik</li>
+            <li>Vishvaraj</li>
+        </ol>
+    );
+}
+
+export default Bullet;
