d05c04196c08f60ed8bd86d0391cce33639ab199
harshved <harshved3@gmail.com>
2022-01-05 18:29:14 +0530

    Calculator challenge completed

--- component_introapp/src/Bullet.jsx
+++ component_introapp/src/Bullet.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Calculator, { add, div, mul, sub } from './Calculator';
 
 function Bullet(){
     return (
         <ol>
-            <li>Harsh</li>
-            <li>Vinay</li>
-            <li>Atik</li>
-            <li>Vishvaraj</li>
+            <li>Addition is         : {add(30,3)}</li>
+            <li>Substraction is     : {sub(30,3)}</li>
+            <li>Division is         : {div(40,3).toFixed(2)}</li>
+            <li>Multiplication is   : {mul(30,3)}</li>
         </ol>
     );
 }
