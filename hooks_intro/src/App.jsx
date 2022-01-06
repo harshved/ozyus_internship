df5866f478a32aadbc76ba207cda3ad6be2af56a
harshved <harshved3@gmail.com>
2022-01-06 20:01:21 +0530

    Time updated on click done

--- hooks_intro/src/App.jsx
+++ hooks_intro/src/App.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
+    let time = new Date().toLocaleTimeString();
+    console.log(time);
+    const [currTime, setTime] = useState(time);
     
-    const IncNo = () => {
-        setCount(count + 1);
+    const updateTime = () => {
+        time = new Date().toLocaleTimeString();
+        setTime(time);
     };
 
     return (
         <>
             <div className="div">
-                <h1> {count} </h1>
-                <button className="button" onClick={IncNo}> Click Me </button>
+                <h1> {currTime} </h1>
+                <button className="button" onClick={updateTime}> Click Me </button>
             </div>
         </>
     )
