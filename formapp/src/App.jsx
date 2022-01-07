dfd7006ce8d5c9b3388fba3ec1ab2d135090185f
harshved <harshved3@gmail.com>
2022-01-07 20:06:49 +0530

    short method for form

--- formapp/src/App.jsx
+++ formapp/src/App.jsx
@@ -11,35 +11,40 @@ const App = () => {
         const {value, name} = event.target;
 
         setvalue((preValue) => {
-            if(name === "fname"){
-                return{
-                    fname: value,
-                    lname: preValue.lname,
-                    email: preValue.email,
-                    phone: preValue.phone,
-                };
-            }else if(name === "lname"){
-                return{
-                    fname: preValue.fname,
-                    lname: value,
-                    email: preValue.email,
-                    phone: preValue.phone,
-                };
-            }else if(name === "email"){
-                return{
-                    fname: preValue.fname,
-                    lname: preValue.lname,
-                    email: value,
-                    phone: preValue.phone,
-                };
-            }else if(name === "phone"){
-                return{
-                    fname: preValue.fname,
-                    lname: preValue.lname,
-                    email: preValue.email,
-                    phone: value,
-                };
-            };
+
+            return {
+                ...preValue,
+                [name]: value,
+            }
+            // if(name === "fname"){
+            //     return{
+            //         fname: value,
+            //         lname: preValue.lname,
+            //         email: preValue.email,
+            //         phone: preValue.phone,
+            //     };
+            // }else if(name === "lname"){
+            //     return{
+            //         fname: preValue.fname,
+            //         lname: value,
+            //         email: preValue.email,
+            //         phone: preValue.phone,
+            //     };
+            // }else if(name === "email"){
+            //     return{
+            //         fname: preValue.fname,
+            //         lname: preValue.lname,
+            //         email: value,
+            //         phone: preValue.phone,
+            //     };
+            // }else if(name === "phone"){
+            //     return{
+            //         fname: preValue.fname,
+            //         lname: preValue.lname,
+            //         email: preValue.email,
+            //         phone: value,
+            //     };
+            // };
         })
     }
     const onSubmit = (event) => {
@@ -54,12 +59,12 @@ const App = () => {
         <>
             <form onSubmit={onSubmit}>
                 <div>
-                    <h1>Hello, {value.fname} {value.lname} {value.email}</h1>,
-                    <p id="detail"></p>,
-                    <input type="text" placeholder="Enter Your First Name" onChange={inputEvent} name="fname" value={value.fname}/>,
-                    <input type="text" placeholder="Enter Your Second Name" onChange={inputEvent} name="lname" value={value.lname}/>,
-                    <input type="email" placeholder="Enter Your Email" onChange={inputEvent} name="email" value={value.email}/>,
-                    <input type="number" placeholder="Enter Your Number" onChange={inputEvent} name="phone" value={value.phone}/>,
+                    <h1>Hello, {value.fname} {value.lname} {value.email}</h1>
+                    <p id="detail"></p>
+                    <input type="text" placeholder="Enter Your First Name" onChange={inputEvent} name="fname" value={value.fname}/>
+                    <input type="text" placeholder="Enter Your Second Name" onChange={inputEvent} name="lname" value={value.lname}/>
+                    <input type="email" placeholder="Enter Your Email" onChange={inputEvent} name="email" value={value.email}/>
+                    <input type="number" placeholder="Enter Your Number" onChange={inputEvent} name="phone" value={value.phone}/>
                     <button type="submit">Click Me 👍</button>
                 </div>
             </form>
