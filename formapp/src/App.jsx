b0c7db19235c7b9fdc450e0a9c43bb84546598c0
harshved <harshved3@gmail.com>
2022-01-07 18:53:34 +0530

    Form with 3 input completed

--- formapp/src/App.jsx
+++ formapp/src/App.jsx
@@ -1,27 +1,51 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [fname, setfname] = useState("");
-    const [lname, setlname] = useState("");
-    const [fullname, setfullname] = useState("");
-    const changeEvent = (event) => {
-        setfname(event.target.value);
-    }
-    const changeEvent1 = (event) => {
-        setlname(event.target.value);
+    const [value, setvalue] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+    });
+    const inputEvent = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+
+        setvalue((preValue) => {
+            if(name === "fname"){
+                return{
+                    fname: value,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                };
+            }else if(name === "lname"){
+                return{
+                    fname: preValue.fname,
+                    lname: value,
+                    email: preValue.email,
+                };
+            }else if(name === "email"){
+                return{
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                };
+            };
+        })
     }
     const onSubmit = (event) => {
         event.preventDefault();
-        setfullname(fname +" "+ lname);
+        alert("Form Submitted Successfully")
     }
+
 
     return (
         <>
             <form onSubmit={onSubmit}>
                 <div>
-                    <h1>Hello, {fullname}</h1>,
-                    <input type="text" placeholder="Enter Your First Name" onChange={changeEvent} value={fname}/>,
-                    <input type="text" placeholder="Enter Your Second Name" onChange={changeEvent1} value={lname}/>,
+                    <h1>Hello, {value.fname} {value.lname} {value.email}</h1>,
+                    <input type="text" placeholder="Enter Your First Name" onChange={inputEvent} name="fname" value={value.fname}/>,
+                    <input type="text" placeholder="Enter Your Second Name" onChange={inputEvent} name="lname" value={value.lname}/>,
+                    <input type="email" placeholder="Enter Your Email" onChange={inputEvent} name="email" value={value.email}/>,
                     <button type="submit">Click Me 👍</button>
                 </div>
             </form>
