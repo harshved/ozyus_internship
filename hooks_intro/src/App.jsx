172c8dfec9691b0ef510266d05152a277f16f723
harshved <harshved3@gmail.com>
2022-01-07 17:05:38 +0530

    Change

--- hooks_intro/src/App.jsx
+++ hooks_intro/src/App.jsx
@@ -3,19 +3,18 @@ import React, { useState } from "react";
 const App = () => {
     // const [count, setCount] = useState(0);
     let time = new Date().toLocaleTimeString();
-    console.log(time);
+    
     const [currTime, setTime] = useState(time);
     
     const updateTime = () => {
         time = new Date().toLocaleTimeString();
         setTime(time);
     };
-
+    setInterval(updateTime, 1000);
     return (
         <>
             <div className="div">
                 <h1> {currTime} </h1>
-                <button className="button" onClick={updateTime}> Click Me </button>
             </div>
         </>
     )
