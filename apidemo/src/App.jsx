fbd882cad17a7efa721b9827b117dee7ce264154
harshved <harshved3@gmail.com>
2022-01-16 17:09:22 +0530

    Fetch data from API

---
+++ apidemo/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Statewise from "./Components/StateWise/stateWise";
+
+const App = () => {
+    return (
+        <>
+            <Statewise/>
+        </>
+    )
+}
+export default App;
