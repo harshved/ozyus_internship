8523cbfca6f960683674f158d62b2ac8786965a2
harshved <harshved3@gmail.com>
2022-01-17 18:37:58 +0530

    useNavigate implemented

--- routerapp/src/User.jsx
+++ routerapp/src/User.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams, useNavigate} from "react-router-dom";
 
 const User = () => {
     const {userName, userLastName} = useParams();
+    const location = useLocation();
+    const navigate = useNavigate();
     return(
         <>
             <h1>I'm from {userName} {userLastName} User Page</h1>
+            <p> My Current location is {location.pathname}</p>
+            {location.pathname === `/user/harsh/ved` ? <button onClick={() => navigate('/')}>Click Me</button> : null}
         </>
     )
 }
